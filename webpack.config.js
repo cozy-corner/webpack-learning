43ce33a4af9eb3207d42e1d164f438dea6bc51f7
cozy <sasakicozy@gmail.com>
2019-11-24 14:33:56 +0900

    implemented getting started after webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+const path = require('path')
+
+// 絶対パスの生成
+const outputpath = path.resolve(__dirname, 'dist')
+
+
+// module とはファイルのこと
+module.exports = {
+  // エントリポイント=バンドルの対象
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path:outputpath
+  } 
+}
