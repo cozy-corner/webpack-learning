ead2ee50194016fe393111a016735d7354517835
cozy <sasakicozy@gmail.com>
2019-11-25 16:19:49 +0900

    implemented babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 // 絶対パスの生成
 const outputpath = path.resolve(__dirname, 'dist') 
@@ -15,6 +16,11 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.jsx?$/, 
+        // 自作のJSのみ対象
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      },
       {
         test: /\.css/,
         use: [
@@ -40,10 +46,20 @@ module.exports = {
           limit: 2048,
           name: './images/[name].[ext]'
         }
+      },
+      {
+         test: /\.html/,
+         loader: 'html-loader'
       }
     ]
   },
   devServer: {
     contentBase: outputpath
-  }
+  },
+  plugins: [
+     new HtmlWebPackPlugin({
+       template: './src/index.html',
+       filename: './index.html'
+     })
+  ]
 }
